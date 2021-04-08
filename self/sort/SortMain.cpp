4fb8f1db4d6a3283796bc08cd6351156d7af6349
llCnll <972581592@qq.com>
2021-04-09 01:00:37 +0800

    feat(排序): 选择/插入/冒泡 以及其优化.

--- self/sort/SortMain.cpp
+++ self/sort/SortMain.cpp
@@ -4,16 +4,124 @@
 
 #define MAX 100000
 
+void SelectSort(int arr[], int max);
+void InsertSort(int arr[], int max);
+void InsertSort02(int arr[], int max);
+void BubbleSort(int arr[], int max);
+void BubbleSort02(int arr[], int max);
+
 int main() {
 
-    int arr1[MAX];
-    int arr2[MAX];
+    int arr01[MAX];
+    int arr02[MAX];
+    int arr03[MAX];
+    int arr04[MAX];
+    int arr05[MAX];
 
-    GetRandOrderArray(arr1, MAX, 0, 100000);
-    CopyArray(arr2, arr1, MAX);
+    //GetRandOrderArray(arr01, MAX, 0, MAX);
+    GetNearlyOrderArray(arr01, MAX, MAX/100);
+    CopyArray(arr02, arr01, MAX);
+    CopyArray(arr03, arr01, MAX);
+    CopyArray(arr04, arr01, MAX);
+    CopyArray(arr05, arr01, MAX);
 
-    printf("%d\t%d\t%d\t\n", arr1[0], arr1[1000], arr1[10000]);
-    printf("%d\t%d\t%d\t\n", arr2[0], arr2[1000], arr2[10000]);
+
+    SortTemplate((char*)"选择排序", SelectSort, arr01, MAX, arr01);
+    SortTemplate((char*)"简单插入排序", InsertSort, arr02, MAX, arr01);
+    SortTemplate((char*)"优化插入排序", InsertSort02, arr03, MAX, arr01);
+    SortTemplate((char*)"简单冒泡排序", BubbleSort, arr04, MAX, arr01);
+    SortTemplate((char*)"优化插入排序", BubbleSort02, arr05, MAX, arr01);
 
     return 0;
+}
+
+/**
+ * @brief 优化冒泡排序
+ *        (若此次冒泡无比较则提前结束)
+ * @param arr 数组
+ * @param max 数组长度
+ *
+ * @return
+ */
+void BubbleSort02(int arr[], int max) {
+    int flag = 1;
+    for (int i = max-1; i > 0 && flag; i--) {
+        flag = 0;
+        for (int j = 0; j < i ; j++) {
+            if (arr[j] > arr[j+1]) {
+                Swap(arr, j, j+1);
+                flag = 1;
+            }
+        }
+    }
+}
+
+/**
+ * @brief 简单冒泡排序
+ * @param arr 数组
+ * @param max 数组长度
+ *
+ * @return
+ */
+void BubbleSort(int arr[], int max) {
+    for (int i = max-1; i > 0; i--) {
+        for (int j = 0; j < i ; j++) {
+            if (arr[j] > arr[j+1]) {
+                Swap(arr, j, j+1);
+            }
+        }
+    }
+}
+
+/**
+ * @brief 优化插入排序
+ *        减少交换次数
+ * @param arr 数组
+ * @param max 数组长度
+ *
+ * @return
+ */
+void InsertSort02(int arr[], int max) {
+    for (int i = 0; i < max-1; ++i) {
+        int j = i+1;
+        int temp = arr[j];
+        for (; j > 0 && arr[j-1] > temp; j--) {
+            arr[j] = arr[j-1];
+        }
+        arr[j] = temp;
+    }
+}
+
+/**
+ * @brief 简单插入排序
+ * @param arr 数组
+ * @param max 数组长度
+ *
+ * @return
+ */
+void InsertSort(int arr[], int max) {
+    for (int i = 0; i < max-1; ++i) {
+        for (int j = i+1; j > 0 && arr[j-1] > arr[j]; j--) {
+            Swap(arr, j, j-1);
+        }
+    }
+}
+
+/**
+ * @brief 选择排序
+ * @param arr 数组
+ * @param max 数组长度
+ *
+ * @return
+ */
+void SelectSort(int arr[], int max) {
+    for (int i = 0; i < max-1; ++i) {
+        int tempIndex = i;
+        for (int j = i+1; j < max; ++j) {
+            if (arr[j] < arr[tempIndex]) {
+                tempIndex = j;
+            }
+        }
+        Swap(arr, i, tempIndex);
+    }
 }
--- self/sort/SortTools.h
+++ self/sort/SortTools.h
@@ -69,6 +69,23 @@ void GetNearlyOrderArray(int arr[], int max, int swapTimes) {
 }
 
 /**
+ * @brief 输出数组(如果数组长度大于100则不输出)
+ * @param arr 数组
+ * @param max 数组的长度
+ *
+ * @return
+ */
+void PrintArray(int arr[], int max) {
+    if (max > 100) {
+        return;
+    }
+    for (int i = 0; i < max; ++i) {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+}
+
+/**
  * @brief 判断数组是否有序
  * @param arr 数组
  * @param max 数组的长度
@@ -85,15 +102,33 @@ int IsSort(int arr[], int max) {
 }
 
 /**
+ * @brief 判断两个数组是否相同
+ * @param arr1 数组
+ * @param arr2 数组
+ * @param max 数组的长度
+ *
+ * @return 1-有序, 0-无序
+ */
+int IsSame(int arr1[], int arr2[], int max) {
+    for (int i = 0; i < max - 1; ++i) {
+        if (arr1[i] != arr2[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+/**
  * @brief 排序模板
  * @param name 排序算法名称
  * @param sort 函数指针
  * @param arr 数组
  * @param max 数组的长度
+ * @param standArr 正确顺序的数组
  *
  * @return
  */
-void SortTemplate(char name[], void(*sort)(int arr[], int max), int arr[], int max) {
+void SortTemplate(char name[], void(*sort)(int arr[], int max), int arr[], int max, int standArr[]) {
     double start, end;
     start = clock();
 
@@ -101,7 +136,10 @@ void SortTemplate(char name[], void(*sort)(int arr[], int max), int arr[], int m
 
     end = clock();
 
-    assert(IsSort(arr, max));
+    PrintArray(arr, max);
 
-    printf("%s's time = %lf\n", name, (end - start) / 1000);
+    assert(IsSort(arr, max));
+    assert(IsSame(arr, standArr, max));
+
+    printf("array.size = %d, %s's time = %lf\n", max, name, (end - start) / 1000);
 }
