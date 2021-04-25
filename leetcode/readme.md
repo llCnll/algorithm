79817db805bbd5e8eafb1ea6a1a52be1ce583ca3
chennan <chennan@aobi.com>
2021-04-25 15:21:16 +0800

    feat: 添加leetcode题集.

---
+++ leetcode/readme.md
@@ -0,0 +1,5 @@
+# leetcode题集
+
+- 辅助工具 run.sh 使用g++快速编译并运行  
+    bash run.sh {file_name}.cpp  
+    run.sh {file_name}.cpp
---
+++ leetcode/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 参数个数
+# echo "$#"
+
+file_full_name=$1
+file_name=${file_full_name%.cpp}
+
+# echo $file_full_name
+# echo $file_name
+
+g++ "$file_full_name" -o "file_name" && "./file_name"
