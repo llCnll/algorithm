e88094da9d07169a37d19ed443bb579482497e00
llCnll <972581592@qq.com>
2021-10-27 09:25:27 +0800

    fix: 修复run.sh生成的程序文件名一致为file_name的问题.

--- leetcode/run.sh
+++ leetcode/run.sh
@@ -8,4 +8,4 @@ file_name=${file_full_name%.cpp}
 # echo $file_full_name
 # echo $file_name
 
-g++ "$file_full_name" -o "file_name" && "./file_name"
+g++ "$file_full_name" -o "$file_name" && "./$file_name"
