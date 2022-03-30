57086d1d01d1f150af043116d77b6e64cc0d7164
llCnll <972581592@qq.com>
2022-03-30 11:43:28 +0800

    feat: add 数组中重复的数据.

--- leetcode/find-all-duplicates-in-an-array.cpp
+++ leetcode/find-all-duplicates-in-an-array.cpp
@@ -4,50 +4,44 @@
 #include <math.h>
 #include <limits.h>
 
-int findDuplicate(int* nums, int numsSize){
-    int slow = 0, fast = 0;
-    do {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-    } while (slow != fast);
-
-    slow = 0;
-
-    while (fast != slow) {
-        slow = nums[slow];
-        fast = nums[fast];
-    }
-    return slow;
-}
-
-/*
-void swap(int* nums, int i, int j) {
+void swap(int * nums, int i, int j) {
     int temp = nums[i];
     nums[i] = nums[j];
     nums[j] = temp;
 }
 
-int findDuplicate(int* nums, int numsSize){
+int* findDuplicates(int* nums, int numsSize, int* returnSize){
+    int * ans = (int *)calloc(numsSize/2, sizeof(int));
+    int count = 0;
 
     int i = 0;
+
     while (i < numsSize) {
         if (nums[i]-1 != i && nums[i] != nums[nums[i]-1]) {
             swap(nums, i, nums[i]-1);
-        } else if (nums[i]-1 != i && nums[i] == nums[nums[i]-1]) return nums[i];
-        else i ++;
+        } else i++;
     }
 
-    for (i = 0; i < numsSize; ++i) printf("%d ", nums[i]);
-    putchar('\n');
-    return 1;
+    for (i = 0; i < numsSize; ++i) {
+        if (nums[i]-1 != i) {
+            ans[count++] = nums[i];
+        }
+    }
+
+    *returnSize = count;
+    return ans;
 }
-*/
+
 int main() {
 
-    int nums[] = {1,3,4,2,2};
-    int numsSize = 5;
+    int nums[] = {4,3,2,7,8,2,3,1};
+    int numsSize = 8;
+    int returnSize = 0;
 
-    printf("%d\n", findDuplicate(nums, numsSize));
+    int * ans = findDuplicates(nums, numsSize, &returnSize);
+    for (int i = 0; i < returnSize; ++i) 
+        printf("%d ", ans[i]);
+    putchar('\n');
 
     return 0;
 }
